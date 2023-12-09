90fd17f2d37a90bba43553e0d38deab97575f2c4
IronBlyzer <adrien.missonnier@gmail.com>
2023-12-09 10:49:28 +0100

    fin parar

--- test.py
+++ test.py
@@ -117,7 +117,7 @@ def change_controls_player2(event):
             print("Appuyez sur une nouvelle touche pour 'Up' de Player 2")
             new_key = wait_for_key()
             player2_controls_azerty["up_key"] = new_key
-
+mouse_x, mouse_y = pygame.mouse.get_pos( )                     
 # Boucle principale
 running = True
 while running:
@@ -135,12 +135,14 @@ while running:
                     angle2 -= 10
                 elif WIDTH * 3 // 4 + 50 <= event.pos[0] <= WIDTH * 3 // 4 + 80 and 235 <= event.pos[1] <= 285:
                     angle2 += 10
-
+                if WIDTH // 2 - 40 < mouse_x < WIDTH // 2 + 40 and HEIGHT - 50 < mouse_y < HEIGHT - 30:
+                    game_state = "menu"
         elif event.type in [pygame.KEYDOWN, pygame.KEYUP]:
             # Appeler la fonction appropriée en fonction du joueur
             if event.type == pygame.KEYDOWN:
                 change_controls_player1(event)
                 change_controls_player2(event)
+                
 
     angle1 %= NUM_COLORS  # Assurez-vous que l'angle reste dans la plage 0-359
     angle2 %= NUM_COLORS
