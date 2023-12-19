7832e7c0d64fe35945e488e1abaf4dbcbf1d88d4
IronBlyzer <adrien.missonnier@gmail.com>
2023-12-19 11:30:02 +0100

    less

--- test.py
+++ test.py
@@ -2,39 +2,31 @@ import pygame
 import sys
 import colorsys
 
-# Initialisation de Pygame
 pygame.init()
 
-# Définition de la taille de la fenêtre
 WIDTH, HEIGHT = 800, 600
 
-# Définition des couleurs
 WHITE = (255, 255, 255)
 BLACK = (0,0,0)
 
-# Définition du nombre de couleurs dans l'arc-en-ciel
 NUM_COLORS = 360
 
-# Définition des angles par défaut
 angle1 = 0
 angle2 = 180
 
-# Contrôles par défaut
 player1_controls_azerty = {"up_key": pygame.K_z, "down_key": pygame.K_s}
 player2_controls_azerty = {"up_key": pygame.K_p, "down_key": pygame.K_m}
 
-# Création de la fenêtre
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Menu de Sélection de Couleurs")
 
 clock = pygame.time.Clock()
 
 
-
     
 def get_rainbow_color(angle):
     """Obtenir une couleur de l'arc-en-ciel en fonction de l'angle."""
-    angle %= NUM_COLORS  # Assurez-vous que l'angle reste dans la plage 0-359
+    angle %= NUM_COLORS  
     rgb = colorsys.hsv_to_rgb(angle / NUM_COLORS, 1.0, 1.0)
     return (int(rgb[0] * 255), int(rgb[1] * 255), int(rgb[2] * 255))
 
@@ -44,29 +36,23 @@ def draw_color_menu(angle, x_position, player_text, controls):
     text_title = font_title.render("Paramètres", True, (255, 255, 255))
     screen.blit(text_title, (400 - text_title.get_width() // 2, 50))
     
-    # Calcul de la couleur de l'arc-en-ciel en fonction de l'angle
     current_color = get_rainbow_color(angle)
 
-    # Affichage du sélecteur de couleurs
     pygame.draw.rect(screen, current_color, (x_position - 50, 235, 100, 50))
 
-    # Affichage du texte du joueur
     font = pygame.font.Font(None, 24)
     text = font.render(player_text, True, (255, 255, 255))
     screen.blit(text, (x_position - text.get_width() // 2, 150))
 
-    # Affichage des boutons de défilement
     pygame.draw.rect(screen, (200, 200, 200), (x_position - 80, 235, 30, 50))  # Bouton gauche
     pygame.draw.rect(screen, (200, 200, 200), (x_position + 50, 235, 30, 50))  # Bouton droit
 
-    # Affichage des labels "Up" et "Down" avec les touches associées
     font = pygame.font.Font(None, 18)
     text_up_key = pygame.key.name(controls.get("up_key", 0))
     text_down_key = pygame.key.name(controls.get("down_key", 0))
     text_up = font.render(f"Up ({text_up_key})", True, (255, 255, 255))
     text_down = font.render(f"Down ({text_down_key})", True, (255, 255, 255))
     
-    # Ajustement des positions pour aligner verticalement
     y_up = 335
     y_down = 355
     
