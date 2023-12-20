d267a69ca15791e71513fa5a9cea388395385d8d
BK <96270359+BK200211@users.noreply.github.com>
2023-12-20 11:52:35 +0100

    Boris

--- pong.py
+++ pong.py
@@ -1,6 +1,9 @@
 import pygame
 import json
 from pygame.locals import *
+import colorsys
+import sys
+
 
 pygame.init()
 
@@ -14,25 +17,39 @@ PADDLE_WIDTH, PADDLE_HEIGHT = 20, 100
 BALL_SIZE = 20
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-GREEN = (0, 0, 255)
+NUM_COLORS = 360
+
+
+player1_y = HEIGHT // 2 - PADDLE_HEIGHT // 2
+player2_y = HEIGHT // 2 - PADDLE_HEIGHT // 2
+ball_x, ball_y = WIDTH // 2, HEIGHT // 2
+ball_speed_x, ball_speed_y = 5, 5
+player1_score = 0
+player2_score = 0
+
+angle1 = 0
+angle2 = 180
 
 # Function to save total wins to a file
 def save_total_wins(total_wins):
     with open('total_wins.json', 'w') as file:
         json.dump({'total_wins': total_wins}, file)
 
-# Function to load total wins from a file
-def load_total_wins():
+# Function to load total wins and colors from a file
+def load_game_data():
     try:
         with open('total_wins.json', 'r') as file:
             data = json.load(file)
-            return data.get('total_wins', 0)
+            return (
+                data.get('total_wins', 0),
+                data.get('player1_color', (255, 0, 0)),
+                data.get('player2_color', (0, 0, 255))
+            )
     except FileNotFoundError:
-        return 0
+        return 0, (255, 0, 0), (0, 0, 255)
 
-# Load total wins at the start of the game
-total_wins = load_total_wins()
+# Load total wins and colors at the start of the game
+total_wins, player1_color, player2_color = load_game_data()
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Pong")
@@ -45,15 +62,15 @@ ball_speed_x, ball_speed_y = 5, 5
 player1_score = 0
 player2_score = 0
 
-player1_color = RED
-player2_color = GREEN
-
 game_state = "menu"
 
 player1_controls_azerty = {pygame.K_z: -5, pygame.K_s: 5}
 player1_controls_qwerty = {pygame.K_w: -5, pygame.K_s: 5}
 player2_controls_azerty = {pygame.K_UP: -5, pygame.K_DOWN: 5}
 player2_controls_qwerty = {pygame.K_w: -5, pygame.K_s: 5}
+
+angle1 = 0
+angle2 = 180
 
 clock = pygame.time.Clock()
 running = True
@@ -87,29 +104,98 @@ def set_game_state(state):
 
 menu_button = Button("Retour", WIDTH // 2 - 60, HEIGHT // 2 + 40, 120, 40, lambda: set_game_state("menu"))
 
+def get_rainbow_color(angle):
+    angle %= NUM_COLORS
+    rgb = colorsys.hsv_to_rgb(angle / NUM_COLORS, 1.0, 1.0)
+    return (int(rgb[0] * 255), int(rgb[1] * 255), int(rgb[2] * 255))
+
+def draw_color_menu(angle, x_position, player_text, controls):
+    current_color = get_rainbow_color(angle)
+
+    pygame.draw.rect(screen, current_color, (x_position - 50, 235, 100, 50))
+
+    font = pygame.font.Font(None, 24)
+    text = font.render(player_text, True, WHITE)
+    screen.blit(text, (x_position - text.get_width() // 2, 150))
+
+    pygame.draw.rect(screen, (200, 200, 200), (x_position - 80, 235, 30, 50))  # Left button
+    pygame.draw.rect(screen, (200, 200, 200), (x_position + 50, 235, 30, 50))  # Right button
+
+    font = pygame.font.Font(None, 18)
+    text_up_key = pygame.key.name(controls.get("up_key", 0))
+    text_down_key = pygame.key.name(controls.get("down_key", 0))
+    text_up = font.render(f"Up ({text_up_key})", True, WHITE)
+    text_down = font.render(f"Down ({text_down_key})", True, WHITE)
+
+    y_up = 335
+    y_down = 355
+
+    screen.blit(text_up, (x_position - text_up.get_width() // 2, y_up))
+    screen.blit(text_down, (x_position - text_down.get_width() // 2, y_down))
+
+def change_controls(controls, event):
+        if event.key == pygame.K_UP:
+            print("Press a new key for 'Up'")
+            new_key = wait_for_key()
+            controls["up_key"] = new_key
+        elif event.key == pygame.K_DOWN:
+            print("Press a new key for 'Down'")
+            new_key = wait_for_key()
+            controls["down_key"] = new_key
+
+def wait_for_key():
+    waiting = True
+    while waiting:
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                return event.key
+
+# Continue with the rest of the code...
+
+running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
-                running = False
-            elif event.key == pygame.K_SPACE:
-                paused = not paused
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if game_state == "menu":
+                if WIDTH // 2 - 40 < event.pos[0] < WIDTH // 2 + 40 and HEIGHT // 2 - 20 < event.pos[1] < HEIGHT // 2 + 10:
+                    game_state = "playing"
+                elif WIDTH // 2 - 70 < event.pos[0] < WIDTH // 2 + 70 and HEIGHT // 2 + 20 < event.pos[1] < HEIGHT // 2 + 60:
+                    game_state = "settings"
+            elif game_state == "settings":
+                if WIDTH // 2 - 40 < event.pos[0] < WIDTH // 2 + 40 and HEIGHT - 50 < event.pos[1] < HEIGHT - 30:
+                    game_state = "menu"
+                else:
+                    # Handle color menu clicks
+                    if WIDTH // 4 - 80 <= event.pos[0] <= WIDTH // 4 - 50 and 235 <= event.pos[1] <= 285:
+                        angle1 -= 10
+                    elif WIDTH // 4 + 50 <= event.pos[0] <= WIDTH // 4 + 80 and 235 <= event.pos[1] <= 285:
+                        angle1 += 10
+                    elif WIDTH * 3 // 4 - 80 <= event.pos[0] <= WIDTH * 3 // 4 - 50 and 235 <= event.pos[1] <= 285:
+                        angle2 -= 10
+                    elif WIDTH * 3 // 4 + 50 <= event.pos[0] <= WIDTH * 3 // 4 + 80 and 235 <= event.pos[1] <= 285:
+                        angle2 += 10
+        if game_state == "playing" : 
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_w] and player1_y > 0:
+                player1_y -= 15
+            if keys[pygame.K_s] and player1_y < HEIGHT - PADDLE_HEIGHT:
+                player1_y += 15
+            if keys[pygame.K_UP] and player2_y > 0:
+                player2_y -= 15
+            if keys[pygame.K_DOWN] and player2_y < HEIGHT - PADDLE_HEIGHT:
+                player2_y += 15
+                
+        elif event.type in [pygame.KEYDOWN, pygame.KEYUP]:
+            if game_state == "settings":
+                change_controls(player1_controls_azerty, event)
+                change_controls(player2_controls_azerty, event)
 
-    if paused:
-        clock.tick(FPS)
-        continue
+    angle1 %= NUM_COLORS
+    angle2 %= NUM_COLORS
 
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_w] and player1_y > 0:
-        player1_y -= 5
-    if keys[pygame.K_s] and player1_y < HEIGHT - PADDLE_HEIGHT:
-        player1_y += 5
-    if keys[pygame.K_UP] and player2_y > 0:
-        player2_y -= 5
-    if keys[pygame.K_DOWN] and player2_y < HEIGHT - PADDLE_HEIGHT:
-        player2_y += 5
+    screen.fill(BLACK)
 
     if game_state == "menu":
         screen.fill(BLACK)
@@ -123,17 +209,24 @@ while running:
         wins_display = font.render(f"Victoires: {total_wins}", True, WHITE)
         screen.blit(wins_display, (10, 10))
 
-        mouse_x, mouse_y = pygame.mouse.get_pos()
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if WIDTH // 2 - 40 < mouse_x < WIDTH // 2 + 40 and HEIGHT // 2 - 20 < mouse_y < HEIGHT // 2 + 10:
-                set_game_state("playing")
-            elif WIDTH // 2 - 70 < mouse_x < WIDTH // 2 + 70 and HEIGHT // 2 + 20 < mouse_y < HEIGHT // 2 + 60:
-                set_game_state("settings")
+
+    elif game_state == "settings":
+        # Draw settings
+        font_title = pygame.font.Font(None, 36)
+        text_title = font_title.render("Settings", True, WHITE)
+        screen.blit(text_title, (WIDTH // 2 - text_title.get_width() // 2, 50))
+
+        draw_color_menu(angle1, WIDTH // 4, "Player 1", player1_controls_azerty)
+        draw_color_menu(angle2, WIDTH * 3 // 4, "Player 2", player2_controls_azerty)
+
+        font_return = pygame.font.Font(None, 26)
+        text_return = font_return.render("RETURN", True, WHITE)
+        screen.blit(text_return, (WIDTH // 2 - text_return.get_width() // 2, HEIGHT - 50))
 
     elif game_state == "playing":
         screen.fill(BLACK)
-        pygame.draw.rect(screen, player1_color, (0, player1_y, PADDLE_WIDTH, PADDLE_HEIGHT))
-        pygame.draw.rect(screen, player2_color, (WIDTH - PADDLE_WIDTH, player2_y, PADDLE_WIDTH, PADDLE_HEIGHT))
+        pygame.draw.rect(screen, get_rainbow_color(angle1), (0, player1_y, PADDLE_WIDTH, PADDLE_HEIGHT))
+        pygame.draw.rect(screen, get_rainbow_color(angle2), (WIDTH - PADDLE_WIDTH, player2_y, PADDLE_WIDTH, PADDLE_HEIGHT))
         pygame.draw.ellipse(screen, WHITE, (ball_x, ball_y, BALL_SIZE, BALL_SIZE))
 
         font = pygame.font.Font(None, 36)
@@ -173,7 +266,7 @@ while running:
 
         if ball_x <= 0:
             player2_score += 1
-            if player2_score == 5:
+            if player2_score == 10:
                 total_wins += 1
                 # Save total wins when a game is finished
                 save_total_wins(total_wins)
@@ -184,7 +277,7 @@ while running:
 
         elif ball_x >= WIDTH - BALL_SIZE:
             player1_score += 1
-            if player1_score == 5:
+            if player1_score == 10:
                 total_wins += 1
                 # Save total wins when a game is finished
                 save_total_wins(total_wins)
@@ -195,6 +288,7 @@ while running:
 
     elif game_state == "settings":
         screen.fill(BLACK)
+        
         font = pygame.font.Font(None, 36)
         back_option = font.render("Retour", True, WHITE)
 
