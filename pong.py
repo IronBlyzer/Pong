7ed76ef09ad78dfca0f85d021d7b2bdff167d73a
BK <96270359+BK200211@users.noreply.github.com>
2023-12-29 19:02:06 +0100

    master

--- pong.py
+++ pong.py
@@ -25,34 +25,34 @@ PADDLE_WIDTH, PADDLE_HEIGHT = 20, 100
 BALL_SIZE = 20
 
 def set_game_state(state):
-    global game_state, player1_score, player2_score, ball_x, ball_y
+    global game_state, joueur1_score, joueur2_score, ball_x, ball_y
     if state == "menu":
-        player1_score = 0
-        player2_score = 0
+        joueur1_score = 0
+        joueur2_score = 0
         ball_x, ball_y = WIDTH // 2, HEIGHT // 2
         game_state = state
-    elif state == "playing":
+    elif state == "joué":
         game_state = state
-    elif state == "winner_message":
+    elif state == "gagnant":
         game_state = state
-    elif state == "settings":
+    elif state == "paramètres":
         game_state = state
-    elif state == "shop":
+    elif state == "boutique":
         game_state = state
 
-player1_y = HEIGHT // 2 - PADDLE_HEIGHT // 2
-player2_y = HEIGHT // 2 - PADDLE_HEIGHT // 2
+joueur1_y = HEIGHT // 2 - PADDLE_HEIGHT // 2
+joueur2_y = HEIGHT // 2 - PADDLE_HEIGHT // 2
 ball_x, ball_y = WIDTH // 2, HEIGHT // 2
 ball_speed_x, ball_speed_y = 5, 5
-player1_score = 0
-player2_score = 0
+joueur1_score = 0
+joueur2_score = 0
 vitpad = 10
-angle1 = 0
-angle2 = 180
-player1_controls_Up = pygame.K_a
-player1_controls_Down = pygame.K_q
-player2_controls_Down = pygame.K_m
-player2_controls_Up = pygame.K_p
+couleur_joueur1 = 0
+couleur_joueur2 = 180
+joueur1_controls_Up = pygame.K_a
+joueur1_controls_Down = pygame.K_q
+joueur2_controls_Down = pygame.K_m
+joueur2_controls_Up = pygame.K_p
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Pong")
 
@@ -61,7 +61,7 @@ winner_display_time = 0
 winner_display_duration = 15000 
 
 repertoire_images = "images"
-shop_images = [f for f in os.listdir(repertoire_images) if f.endswith(('.png', '.jpg', '.jpeg'))]
+boutique_images = [f for f in os.listdir(repertoire_images) if f.endswith(('.png', '.jpg', '.jpeg'))]
 selected_image = None
 
 background_images_folder = "images"
@@ -73,13 +73,13 @@ def get_random_background():
 
 
 
-def draw_shop():
+def draw_boutique():
     screen.fill(BLACK)
     font = pygame.font.Font(None, 36)
-    shop_title = font.render("Image Shop", True, WHITE)
-    screen.blit(shop_title, (WIDTH // 2 - shop_title.get_width() // 2, 50))
+    boutique_title = font.render("Image boutique", True, WHITE)
+    screen.blit(boutique_title, (WIDTH // 2 - boutique_title.get_width() // 2, 50))
 
-    for i, image in enumerate(shop_images):
+    for i, image in enumerate(boutique_images):
         image_surface = pygame.image.load(os.path.join(repertoire_images, image))
         image_surface = pygame.transform.scale(image_surface, (100, 100))
         screen.blit(image_surface, (50, 150 + i * 120))
@@ -97,12 +97,12 @@ def draw_return_button():
     pygame.display.flip()
 
 
-def save_game_data(points, angle1, angle2):
+def save_game_data(points, couleur_joueur1, couleur_joueur2):
     with open('points.json', 'w') as file:
         json.dump({
             'points': points,
-            'angle1': angle1,
-            'angle2': angle2
+            'couleur_joueur1': couleur_joueur1,
+            'couleur_joueur2': couleur_joueur2
         }, file)
 
 class Button:
@@ -124,13 +124,13 @@ def load_game_data():
             data = json.load(file)
             return (
                 data.get('points', 0),
-                data.get('angle1', angle1),
-                data.get('angle2', angle2)
+                data.get('couleur_joueur1', couleur_joueur1),
+                data.get('couleur_joueur2', couleur_joueur2)
             )
     except FileNotFoundError:
-        return 0, angle1, angle2
+        return 0, couleur_joueur1, couleur_joueur2
 
-points, angle1, angle2 = load_game_data()
+points, couleur_joueur1, couleur_joueur2 = load_game_data()
 
 def reset_ball():
     return WIDTH // 2, HEIGHT // 2
@@ -170,28 +170,28 @@ while running:
         elif event.type == pygame.MOUSEBUTTONDOWN:
             if game_state == "menu":
                 if WIDTH // 2 - 40 < event.pos[0] < WIDTH // 2 + 40 and HEIGHT // 2 - 20 < event.pos[1] < HEIGHT // 2 + 10:
-                    set_game_state("playing")
+                    set_game_state("joué")
                 elif WIDTH // 2 - 70 < event.pos[0] < WIDTH // 2 + 70 and HEIGHT // 2 + 20 < event.pos[1] < HEIGHT // 2 + 60:
-                    set_game_state("settings")
-                elif WIDTH - 120 <= event.pos[0] <= WIDTH - 120 + shop_button.rect.width and 10 <= event.pos[1] <= 10 + shop_button.rect.height:
-                    set_game_state("shop")
-            elif game_state == "settings":
+                    set_game_state("paramètres")
+                elif WIDTH - 120 <= event.pos[0] <= WIDTH - 120 + boutique_button.rect.width and 10 <= event.pos[1] <= 10 + boutique_button.rect.height:
+                    set_game_state("boutique")
+            elif game_state == "paramètres":
                 if WIDTH // 2 - 40 < event.pos[0] < WIDTH // 2 + 40 and HEIGHT - 50 < event.pos[1] < HEIGHT - 30:
-                    save_game_data(points, angle1, angle2)
+                    save_game_data(points, couleur_joueur1, couleur_joueur2)
                     set_game_state("menu")
                 else:
                     if WIDTH // 4 - 80 <= event.pos[0] <= WIDTH // 4 - 50 and 235 <= event.pos[1] <= 285:
-                        angle1 -= 10
+                        couleur_joueur1 -= 10
                     elif WIDTH // 4 + 50 <= event.pos[0] <= WIDTH // 4 + 80 and 235 <= event.pos[1] <= 285:
-                        angle1 += 10
+                        couleur_joueur1 += 10
                     elif WIDTH * 3 // 4 - 80 <= event.pos[0] <= WIDTH * 3 // 4 - 50 and 235 <= event.pos[1] <= 285:
-                        angle2 -= 10
+                        couleur_joueur2 -= 10
                     elif WIDTH * 3 // 4 + 50 <= event.pos[0] <= WIDTH * 3 // 4 + 80 and 235 <= event.pos[1] <= 285:
-                        angle2 += 10
-                    save_game_data(points, angle1, angle2)
-            elif game_state == "shop":
+                        couleur_joueur2 += 10
+                    save_game_data(points, couleur_joueur1, couleur_joueur2)
+            elif game_state == "boutique":
                 x, y = event.pos
-                for i, image in enumerate(shop_images):
+                for i, image in enumerate(boutique_images):
                     if 50 <= x <= 150 and 150 + i * 120 <= y <= 150 + (i + 1) * 120:
                         selected_image = image
                         if points >= 5:
@@ -205,8 +205,8 @@ while running:
                 if WIDTH // 2 - 60 <= x <= WIDTH // 2 + 60 and HEIGHT - 50 <= y <= HEIGHT - 10:
                     set_game_state("menu")
 
-    angle1 %= NUM_COLORS
-    angle2 %= NUM_COLORS
+    couleur_joueur1 %= NUM_COLORS
+    couleur_joueur2 %= NUM_COLORS
 
     if background_image is None:
         background_image = get_random_background()
@@ -217,28 +217,28 @@ while running:
     if game_state == "menu":
         font = pygame.font.Font(None, 36)
         play_option = font.render("Jouer", True, WHITE)
-        settings_option = font.render("Paramètres", True, WHITE)
-        shop_option = font.render("Boutique", True, WHITE)  
-        shop_button = Button("Boutique", WIDTH - 120, 10, shop_option.get_width(), shop_option.get_height(), lambda: set_game_state("shop"))
-        screen.blit(shop_option, (WIDTH - 120, 10))
+        paramètres_option = font.render("Paramètres", True, WHITE)
+        boutique_option = font.render("Boutique", True, WHITE)  
+        boutique_button = Button("Boutique", WIDTH - 120, 10, boutique_option.get_width(), boutique_option.get_height(), lambda: set_game_state("boutique"))
+        screen.blit(boutique_option, (WIDTH - 120, 10))
         screen.blit(play_option, (WIDTH // 2 - 40, HEIGHT // 2 - 20))
-        screen.blit(settings_option, (WIDTH // 2 - 70, HEIGHT // 2 + 20))
+        screen.blit(paramètres_option, (WIDTH // 2 - 70, HEIGHT // 2 + 20))
         wins_display = font.render(f"Points: {points}", True, WHITE)
         screen.blit(wins_display, (10, 10))
 
-    elif game_state == "settings":
+    elif game_state == "paramètres":
         font_title = pygame.font.Font(None, 36)
         text_title = font_title.render("Paramètres", True, WHITE)
         screen.blit(text_title, (WIDTH // 2 - text_title.get_width() // 2, 50))
 
-        draw_color_menu(angle1, WIDTH // 4, "Joeur 1")
-        draw_color_menu(angle2, WIDTH * 3 // 4, "Joeur 2")
+        draw_color_menu(couleur_joueur1, WIDTH // 4, "Joeur 1")
+        draw_color_menu(couleur_joueur2, WIDTH * 3 // 4, "Joeur 2")
         
         font = pygame.font.Font(None, 18)
-        text_up1 = font.render(f"Up ({pygame.key.name(player1_controls_Up)})", True, WHITE)
-        text_down1 = font.render(f"Down ({pygame.key.name(player1_controls_Down)})", True, WHITE)
-        text_up2 = font.render(f"Up ({pygame.key.name(player2_controls_Up)})", True, WHITE)
-        text_down2 = font.render(f"Down ({pygame.key.name(player2_controls_Down)})", True, WHITE)
+        text_up1 = font.render(f"Up ({pygame.key.name(joueur1_controls_Up)})", True, WHITE)
+        text_down1 = font.render(f"Down ({pygame.key.name(joueur1_controls_Down)})", True, WHITE)
+        text_up2 = font.render(f"Up ({pygame.key.name(joueur2_controls_Up)})", True, WHITE)
+        text_down2 = font.render(f"Down ({pygame.key.name(joueur2_controls_Down)})", True, WHITE)
         y_up = 335
         y_down = 355
         screen.blit(text_up1, (WIDTH // 4 - text_up1.get_width() // 2, y_up))
@@ -250,30 +250,30 @@ while running:
         text_return = font_return.render("Retour", True, WHITE)
         screen.blit(text_return, (WIDTH // 2 - text_return.get_width() // 2, HEIGHT - 50))
 
-    elif game_state == "shop":
-        draw_shop()
+    elif game_state == "boutique":
+        draw_boutique()
         font_return = pygame.font.Font(None, 26)
         text_return = font_return.render("Retour", True, WHITE)
         screen.blit(text_return, (WIDTH // 2 - text_return.get_width() // 2, HEIGHT - 50))
 
-    elif game_state == "playing":
-        pygame.draw.rect(screen, get_rainbow_color(angle1), (0, player1_y, PADDLE_WIDTH, PADDLE_HEIGHT))
-        pygame.draw.rect(screen, get_rainbow_color(angle2), (WIDTH - PADDLE_WIDTH, player2_y, PADDLE_WIDTH, PADDLE_HEIGHT))
+    elif game_state == "joué":
+        pygame.draw.rect(screen, get_rainbow_color(couleur_joueur1), (0, joueur1_y, PADDLE_WIDTH, PADDLE_HEIGHT))
+        pygame.draw.rect(screen, get_rainbow_color(couleur_joueur2), (WIDTH - PADDLE_WIDTH, joueur2_y, PADDLE_WIDTH, PADDLE_HEIGHT))
         pygame.draw.ellipse(screen, WHITE, (ball_x, ball_y, BALL_SIZE, BALL_SIZE))
 
         font = pygame.font.Font(None, 36)
-        score_display = font.render(f"{player1_score} - {player2_score}", True, WHITE)
+        score_display = font.render(f"{joueur1_score} - {joueur2_score}", True, WHITE)
         screen.blit(score_display, (WIDTH // 2 - 40, 20))
 
         keys = pygame.key.get_pressed()
-        if keys[player1_controls_Up] and player1_y > 0:
-            player1_y -= vitpad
-        elif keys[player1_controls_Down] and player1_y < HEIGHT - PADDLE_HEIGHT:
-            player1_y += vitpad
-        elif keys[player2_controls_Down] and player2_y < HEIGHT - PADDLE_HEIGHT:
-            player2_y += vitpad
-        elif keys[player2_controls_Up] and player2_y > 0:
-            player2_y -= vitpad
+        if keys[joueur1_controls_Up] and joueur1_y > 0:
+            joueur1_y -= vitpad
+        elif keys[joueur1_controls_Down] and joueur1_y < HEIGHT - PADDLE_HEIGHT:
+            joueur1_y += vitpad
+        elif keys[joueur2_controls_Down] and joueur2_y < HEIGHT - PADDLE_HEIGHT:
+            joueur2_y += vitpad
+        elif keys[joueur2_controls_Up] and joueur2_y > 0:
+            joueur2_y -= vitpad
 
         ball_x += ball_speed_x
         ball_y += ball_speed_y
@@ -282,35 +282,35 @@ while running:
             ball_speed_y = -ball_speed_y
 
         if (
-            (ball_x <= PADDLE_WIDTH and player1_y <= ball_y <= player1_y + PADDLE_HEIGHT)
-            or (ball_x >= WIDTH - PADDLE_WIDTH - BALL_SIZE and player2_y <= ball_y <= player2_y + PADDLE_HEIGHT)
+            (ball_x <= PADDLE_WIDTH and joueur1_y <= ball_y <= joueur1_y + PADDLE_HEIGHT)
+            or (ball_x >= WIDTH - PADDLE_WIDTH - BALL_SIZE and joueur2_y <= ball_y <= joueur2_y + PADDLE_HEIGHT)
         ):
             ball_speed_x = -ball_speed_x
 
         if ball_x <= 0:
-            player2_score += 1
-            if player2_score == 5:
+            joueur2_score += 1
+            if joueur2_score == 5:
                 points += 1
-                save_game_data(points, angle1, angle2)
+                save_game_data(points, couleur_joueur1, couleur_joueur2)
                 winner_display_time = pygame.time.get_ticks()
-                set_game_state("winner_message")
+                set_game_state("gagnant")
             else:
                 ball_x, ball_y = reset_ball()
 
         elif ball_x >= WIDTH - BALL_SIZE:
-            player1_score += 1
-            if player1_score == 5:
+            joueur1_score += 1
+            if joueur1_score == 5:
                 points += 1
-                save_game_data(points, angle1, angle2)
+                save_game_data(points, couleur_joueur1, couleur_joueur2)
                 winner_display_time = pygame.time.get_ticks()
-                set_game_state("winner_message")
+                set_game_state("gagnant")
             else:
                 ball_x, ball_y = reset_ball()
 
-    elif game_state == "winner_message":
+    elif game_state == "gagnant":
         screen.fill(BLACK)
         font = pygame.font.Font(None, 36)
-        winner_text = f"Joueur {1 if player1_score == 5 else 2} gagne!"
+        winner_text = f"Joueur {1 if joueur1_score == 5 else 2} gagne!"
         winner_display = font.render(winner_text, True, WHITE)
         winner_rect = winner_display.get_rect(center=(WIDTH // 2, HEIGHT // 2))
         screen.blit(winner_display, winner_rect)
@@ -321,7 +321,7 @@ while running:
             if menu_button.rect.collidepoint(mouse_x, mouse_y):
                 set_game_state("menu")
 
-    if game_state == "winner_message" and winner_display_time > 0 and pygame.time.get_ticks() - winner_display_time > winner_display_duration:
+    if game_state == "gagnant" and winner_display_time > 0 and pygame.time.get_ticks() - winner_display_time > winner_display_duration:
         set_game_state("menu")
         winner_display_time = 0
 
